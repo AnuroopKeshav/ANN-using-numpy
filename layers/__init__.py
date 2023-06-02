83b86dffec35e26c1e6214fccb6d318740d6e6fd
AnuroopKeshav <keshavanuroop@gmail.com>
2023-06-02 12:09:18 +0530

    BAK

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,2 +1,6 @@
+from layers.base import Base
 from layers.dense import Dense
 from layers.dropout import Dropout
+from layers.flatten import Flatten
+from layers.conv2d import Conv2d
+from layers.conv3d import Conv3d
---
+++ layers/base.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+class Base:
+    self __init__ (self):
+        pass
+
